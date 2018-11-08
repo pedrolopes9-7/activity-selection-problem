0082f62e4976ef17a7615430e68bd1ad7bbbbd02
pedrolopes9-7 <pedroinf97@gmail.com>
2018-11-08 13:13:35 -0600

    #fix algoritmo guloso. #fix gerador de instancias - com erro

--- Main.java
+++ Main.java
@@ -6,9 +6,11 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Collections;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
-	public static final String ABSOLUTE_PATH = "/home/sigma/Documentos/activity-selection-problem/instances/";
+	public static final String ABSOLUTE_PATH = "instances/";
+	//public static final String ABSOLUTE_PATH = "/home/sigma/Documentos/activity-selection-problem/instances/";
 	public static final int FILE_SIZE = 10;
 	public static final int FILE_QNT = 10;
 
@@ -18,25 +20,33 @@ public class Main {
 		List<Activity> output = null;
 
 		System.out.println("---------Activity Selection Problem---------\n");
-		System.out.println("Setup: java Main -i <file_name>.txt -g|-d|-b");
+		System.out.println("Setup: java Main -i <file_name> -g|-d|-b");
 		System.out.println("-i <file_name> to run on instance <file_name>.txt \n" +
 							"-g to run Greedy Heuristic.\n" +
 							"-d to run Dynamic Programming Heuristic\n" +
 							"-b to run Backtracking Heuristic\n");
 
-		//generateInstances(FILE_QNT, FILE_SIZE);
-
+		/* programa principal: le uma instancia do problem especificada no args[1] e executa algum
+			algoritmo, especificado em args[2], para o problema de selecao de atividades*/ 
 		if (args[0].equals("-i")){
 			fileName = ABSOLUTE_PATH + args[1] + ".txt";
 			readInstances(fileName, listActivities, FILE_SIZE);
-		}
 
-		if (args[2].equals("-g")){
-			output = greedyAlgorithm(listActivities);
-			System.out.println("Seleção de atividades compatíveis pelo algoritmo guloso:");
-			for (Activity ac : output){
-				System.out.println(ac);
+			if (args[2].equals("-g")){
+				output = greedyAlgorithm(listActivities);
+				System.out.println("Seleção de atividades compatíveis pelo algoritmo guloso:");
+				for (Activity ac : output){
+					System.out.println(ac);
+				}
+			}else if (args[2].equals("-d")){
+			//RUN dynamic programming algorithm
+			}else if (args[2].equals("-b")){
+			//RUN backtracking algorithm
 			}
+
+		/* opcao de gerar as intancias do problema*/
+		}else if (args[0].equals("-k")){
+			generateInstances(FILE_QNT, FILE_SIZE);
 		}
 	}
 
@@ -55,9 +65,9 @@ public class Main {
 			endTimes.add(activities.get(k).getEndTime());
 		}
 
-		//Escolha gulosa - checa se o tempo de inicio da próxima atividade é maior ou igual ao tempo de 
-		//finalizacao da utlima atividade inserida na sub-estrutura otima
-		for (j = 1; i < activities.size(); i++){
+		/* Escolha gulosa - checa se o tempo de inicio da próxima atividade é maior ou igual ao tempo de 
+		finalizacao da utlima atividade inserida na sub-estrutura otima */
+		for (j = 1; j < activities.size(); j++){
 			if (startTimes.get(j) >= endTimes.get(i)){
 				selectedActivities.add(activities.get(j));
 				i = j;
@@ -66,26 +76,33 @@ public class Main {
 		return selectedActivities;
 	}
 
-	// public static List<Activity> DP_Algorithm(List<Activity> activities){
-
-	// }
+	public static List<Activity> DP_Algorithm(List<Activity> activities){
+		List<Activity> list = new ArrayList<Activity>();
+		return list;
+	}
 
 	public static void generateInstances(int fileQnt, int fileSize){
 		File out = null;
 		List<FileWriter> listWriter = new ArrayList<FileWriter>(fileQnt);
 
-		//TODO for tá rodando só uma vez sabe-se lá pq. só um arquivo tá sendo gerado.
-		//TODO gerar valores aleatorios inteiros para as instancias a partir da distribuicao de gauss
+		/* intervalo do numero aleatorio*/
+		final int min = 1, max = 20;
+		int random1 = 0, random2 = 0;;
+
+		/* gera n instancias para o problema. n = FILE_QNT*/
 		for (int i = 0; i < fileQnt; i++){
-			System.out.println(i + "\n");
 			out = new File(ABSOLUTE_PATH + "instance" + i + ".txt");
 
 			try{
 				out.createNewFile();
 				listWriter.add(new FileWriter(out));
 
-				for (int j = 0; i < fileSize; i++){
-					listWriter.get(j).write("1" + " " + "2" + "\n"); 
+				/* gera m linhas de atividades para a instancia. m = FILE_SIZE */
+				for (int j = 0; j < fileSize; j++){
+					random1 = ThreadLocalRandom.current().nextInt(min, max + 1);
+					random2 = ThreadLocalRandom.current().nextInt(min, max + 1);
+					System.out.println(random1 + " " + random2);
+					listWriter.get(j).write(String.valueOf(random1) + " " + String.valueOf(random2) + "\n"); 
 				}
 
 			} catch (IOException ex){
