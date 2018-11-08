c49561dca0d8521289ea2e6e84ea2547b4a6ae48
proativa2018 <projetoproativa2018@gmail.com>
2018-11-08 10:00:01 -0200

    some fixes

--- Main.java
+++ Main.java
@@ -5,11 +5,17 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Collections;
 
 public class Main {
+	public static final String ABSOLUTE_PATH = "/home/sigma/Documentos/activity-selection-problem/instances/";
+	public static final int FILE_SIZE = 10;
+	public static final int FILE_QNT = 10;
+
 	public static void main (String[] args){
 		String fileName = "";
-		List<Activity> list = new ArrayList<Activity>();
+		List<Activity> listActivities = new ArrayList<Activity>();
+		List<Activity> output = null;
 
 		System.out.println("---------Activity Selection Problem---------\n");
 		System.out.println("Setup: java Main -i <file_name>.txt -g|-d|-b");
@@ -18,31 +24,58 @@ public class Main {
 							"-d to run Dynamic Programming Heuristic\n" +
 							"-b to run Backtracking Heuristic\n");
 
-		fileName = "instances/" + args[1] + ".txt";
+		//generateInstances(FILE_QNT, FILE_SIZE);
 
-		/* TODO implementar os 3 métodos de resolucao
-		if (args[2] == "-g"){
-			//Roda algoritmo guloso
-		}else if (args[2] == "-d"){
-			//Roda algoritmo de programação dinâmica
-		}else{
-			//Roda algoritmo de backtracking
+		if (args[0].equals("-i")){
+			fileName = ABSOLUTE_PATH + args[1] + ".txt";
+			readInstances(fileName, listActivities, FILE_SIZE);
 		}
-		*/
 
-		//fileQnt = número de arquivos de instancias
-		//fileSize = tamanho da instancia
-
-		generateInstances(10, 10);
-
+		if (args[2].equals("-g")){
+			output = greedyAlgorithm(listActivities);
+			System.out.println("Seleção de atividades compatíveis pelo algoritmo guloso:");
+			for (Activity ac : output){
+				System.out.println(ac);
+			}
+		}
 	}
 
+	public static List<Activity> greedyAlgorithm(List<Activity> activities){
+		List<Activity> selectedActivities = new ArrayList<Activity>();
+		List<Integer> startTimes = new ArrayList<Integer>();
+		List<Integer> endTimes = new ArrayList<Integer>();
+
+		int i = 0, j;
+
+		//Ordenação crescente por tempo de finalizacao
+		Collections.sort(activities);
+
+		for (int k = 0; k < activities.size(); k++){
+			startTimes.add(activities.get(k).getStartTime());
+			endTimes.add(activities.get(k).getEndTime());
+		}
+
+		//Escolha gulosa - checa se o tempo de inicio da próxima atividade é maior ou igual ao tempo de 
+		//finalizacao da utlima atividade inserida na sub-estrutura otima
+		for (j = 1; i < activities.size(); i++){
+			if (startTimes.get(j) >= endTimes.get(i)){
+				selectedActivities.add(activities.get(j));
+				i = j;
+			}
+		}
+		return selectedActivities;
+	}
+
+	// public static List<Activity> DP_Algorithm(List<Activity> activities){
+
+	// }
+
 	public static void generateInstances(int fileQnt, int fileSize){
-		final String ABSOLUTE_PATH = "/home/sigma/Documentos/activity-selection-problem/instances/";
 		File out = null;
 		List<FileWriter> listWriter = new ArrayList<FileWriter>(fileQnt);
 
 		//TODO for tá rodando só uma vez sabe-se lá pq. só um arquivo tá sendo gerado.
+		//TODO gerar valores aleatorios inteiros para as instancias a partir da distribuicao de gauss
 		for (int i = 0; i < fileQnt; i++){
 			System.out.println(i + "\n");
 			out = new File(ABSOLUTE_PATH + "instance" + i + ".txt");
@@ -76,7 +109,7 @@ public class Main {
 			s = new Scanner(file);
 
 			for (int i = 0; i < fileSize; i++){
-				activities.add(new Activity(s.nextDouble(), s.nextDouble()));
+				activities.add(new Activity(s.nextInt(), s.nextInt()));
 			}
 
 		} catch(IOException ex){
@@ -85,28 +118,39 @@ public class Main {
 	}
 }
 
-class Activity {
-	private double start_time;
-	private double end_time;
+class Activity implements Comparable<Activity>{
+	private int start_time;
+	private int end_time;
 
-	public Activity(double st, double et){
+	public Activity(int st, int et){
 		this.start_time = st;
 		this.end_time = et;
 	}
 
-	public double getStartTime(){
+	public int getStartTime(){
 		return this.start_time;
 	}
 
-	public double getEndTime(){
+	public int getEndTime(){
 		return this.end_time;
 	}
 
-	public void setStartTime(double st){
+	public void setStartTime(int st){
 		this.start_time = st;
 	}
 
-	public void setEndTime(double et){
+	public void setEndTime(int et){
 		this.end_time = et;
+	}
+
+	@Override
+	public int compareTo(Activity activity){
+		int compareEnd_Time = ((Activity) activity).getEndTime();
+		return this.end_time - compareEnd_Time;
+	}
+
+	@Override
+	public String toString(){
+		return "Inicio: " + this.start_time + " " + "Termino: " + this.end_time;
 	}
 }
